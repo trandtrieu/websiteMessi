dbbb0b4712a28f23d18cff252533f8d076f255c5
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2023-01-05 00:26:20 +0700

    up2023

--- mycode.js
+++ mycode.js
@@ -15,3 +15,69 @@ window.onscroll = () => {
   menu.classList.remove("bx-x");
   navlist.classList.remove("open");
 };
+
+$(".step").click(function () {
+  $(this).addClass("active").prevAll().addClass("active");
+  $(this).nextAll().removeClass("active");
+});
+
+$(".step01").click(function () {
+  $("#line-progress").css("width", "3%");
+  $(".discovery").addClass("active").siblings().removeClass("active");
+});
+
+$(".step02").click(function () {
+  $("#line-progress").css("width", "25%");
+  $(".strategy").addClass("active").siblings().removeClass("active");
+});
+
+$(".step03").click(function () {
+  $("#line-progress").css("width", "50%");
+  $(".creative").addClass("active").siblings().removeClass("active");
+});
+
+$(".step04").click(function () {
+  $("#line-progress").css("width", "75%");
+  $(".production").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click(function () {
+  $("#line-progress").css("width", "100%");
+  $(".analysis").addClass("active").siblings().removeClass("active");
+});
+
+function re() {
+  window.location.reload();
+}
+
+const btn = document.querySelector(".btn-toggle");
+const theme = document.querySelector("#theme-link");
+
+// Lắng nghe sự kiện click vào button
+btn.addEventListener("click", function () {
+  // Nếu URL đang là "ligh-theme.css"
+  if (theme.getAttribute("href") == "style.css") {
+    // thì chuyển nó sang "dark-theme.css"
+    theme.href = "dark.css";
+  } else {
+    // và ngược lại
+    theme.href = "style.css";
+  }
+});
+
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
